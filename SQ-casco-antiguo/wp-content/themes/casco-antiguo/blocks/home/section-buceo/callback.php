622066421f38362cd449eb09d2a68a143f9848b9
Fluxel-Code <110497050+Fluxel-Code@users.noreply.github.com>
2023-10-02 15:45:45 -0400

    subido dos archivos

--- SQ-casco-antiguo/wp-content/themes/casco-antiguo/blocks/home/section-buceo/callback.php
+++ SQ-casco-antiguo/wp-content/themes/casco-antiguo/blocks/home/section-buceo/callback.php
@@ -4,21 +4,29 @@
 function FUN_BLOCK_HOME_SectionBuceo____content(){
     ?>
     
-    <p>esto seria una foto o video de fondo</p>
-    <div class="sq-grid-3 sq-gap-40">
-        <div class="item">
-            esta seria la una caja con el contenido
-            <h2>
-                Tu curso de buceo
-            </h2>
-            <p>En Casco Antiguo trabajamos día a día para atender a nuestros clientes con la máxima profesionalidad, ofreciendo calidad, precio y un servicio inmejorables.</p>
-            <div class="boton">
-                <a href="<?php echo get_site_url()."nada"; ?>" class="SQ_btn">Acceder</a>
+        <section id="HOME_BUCEO">
+            <div class="square-buceo">
+                <figure class="m-0 video-divers">
+                    <video src="https://cascoantiguo.temporalweb.es/wp-content/uploads/2023/09/cursobuceo.mp4" muted class="w-100 h-100"></video>
+                </figure>
+                <div class="box-info">
+                    <div class="content">
+                        <h2 class="title-divers text-white">
+                            Tu curso
+                            <br>
+                            de buceo
+                        </h2>
+                        <p class="paragraph text-white">En Casco Antiguo trabajamos día a día para atender a nuestros clientes con la máxima profesionalidad, ofreciendo calidad, precio y un servicio inmejorables.</p>
+                        <div class="boton">
+                            <a href="#" class="SQ_btn d-block">Acceder</a>
+                        </div>
+                    </div>
+                    <!-- content -->
+                </div>
+                <!-- box-info -->
             </div>
-        </div>
-        <div class="item"></div>
-        <div class="item"></div>
-    </div>
+        </section>
+        <!-- #HOME_BUCEO -->
     
     <?php
 }
