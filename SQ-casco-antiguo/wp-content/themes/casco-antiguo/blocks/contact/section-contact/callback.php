72ef96f80ef45bf7f72c0494f02d2e59cd81a137
Fluxel-Code <110497050+Fluxel-Code@users.noreply.github.com>
2023-10-03 15:27:04 -0400

    cambios del block

--- SQ-casco-antiguo/wp-content/themes/casco-antiguo/blocks/contact/section-contact/callback.php
+++ SQ-casco-antiguo/wp-content/themes/casco-antiguo/blocks/contact/section-contact/callback.php
@@ -5,12 +5,12 @@ function FUN_BLOCK_CONTACT_Landing_contacto____content(){
     echo "****";
     ?>
         <section id="CONTACT_FORM" class="wrap-small">
-                <form action="" class="w-100">
-                    <div class="content-form">
-                        <div class="info">
-                            <h1 class="title-contact">Contacta</h1>
-                            <div class="text-icon">
-                                <span>
+            <form action="" class="w-100">
+                <div class="content-form">
+                    <div class="info">
+                        <h1 class="title-contact">Contacta</h1>
+                        <div class="text-icon">
+                            <span>
                                     <svg
                                         xmlns="http://www.w3.org/2000/svg"
                                         width="22.365"
@@ -42,6 +42,62 @@ function FUN_BLOCK_CONTACT_Landing_contacto____content(){
                  </div>
                 <!-- .content-form-->
             </form>
+
+            <div class="content-map">
+                <div class="info">
+                    <div class="part-upper">
+                        <h2 class="title-contact">
+                            Ven a conocernos
+                        </h2>
+                        <div class="location">
+                            <i class="icon-location">
+                                <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="21.443"
+                                        height="21.443"
+                                        viewBox="0 0 21.443 21.443"
+                                    >
+                                        <path
+                                            id="Trazado_1014"
+                                            data-name="Trazado 1014"
+                                            d="M10.721,21.443A10.722,10.722,0,1,1,21.443,10.722,10.723,10.723,0,0,1,10.721,21.443m0-17.7a5.31,5.31,0,0,1,5.311,5.311c0,2.933-5.311,8.637-5.311,8.637s-5.311-5.7-5.311-8.637a5.31,5.31,0,0,1,5.311-5.311m0,3.2A2.014,2.014,0,1,1,8.708,8.962a2.014,2.014,0,0,1,2.014-2.014"
+                                            fill="#72b0cb"
+                                            fill-rule="evenodd"
+                                        />
+                                    </svg>
+                            </i>
+                            <a href="#">
+                                Estamos en Madrid,
+                                <br>
+                                en
+                                <strong>C. de Jorge Juan, 118</strong>
+                            </a>
+                        </div>
+                        <!-- .location -->
+                    </div>
+                    <!-- .part-upper -->
+                    <div class="part-bottom">
+                        <a href="#" class="SQ_btn btn formatier-btn d-block">
+                            Encuéntranos en Google Maps
+                        </a>
+                    </div>
+                    <!-- .part-bottom -->
+                </div>
+                <!-- .info -->
+                <div class="direction">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12149.493185386187!2d-3.676859922158063!3d40.42273226541802!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd422890ac9ebf1b%3A0x8acd6a77b2993452!2sCasco%20Antiguo%20Buceo!5e0!3m2!1ses-419!2sve!4v1696354541122!5m2!1ses-419!2sve"
+                        height="461"
+                        style="border:0;"
+                        allowfullscreen=""
+                        loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"
+                        class="w-100"
+                    ></iframe>
+                </div>
+                <!-- .direction -->
+            </div>
+            <!-- .content-map -->
         </section>
     <?php
 }
