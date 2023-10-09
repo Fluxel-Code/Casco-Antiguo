c5328d7b88f085bf109a601e55d199244fa04742
Fluxel-Code <110497050+Fluxel-Code@users.noreply.github.com>
2023-10-09 17:23:23 -0400

    se subio cabecera
    falta campos personalizados

--- SQ-casco-antiguo/wp-content/themes/casco-antiguo/blocks/instalaciones/section-header/callback.php
+++ SQ-casco-antiguo/wp-content/themes/casco-antiguo/blocks/instalaciones/section-header/callback.php
@@ -4,6 +4,23 @@
 function FUN_BLOCK_INST_SectionHead_instalaciones____content(){
     //Recuerdar poner esta funcion en "___base"
     ?>
-    <p>Section cabecera Instalaciones</p>
+        <figure class="bg-img">
+            <img src="https://cascoantiguo.temporalweb.es/wp-content/uploads/2023/09/PISCINA_1.jpg" alt="" class="img-fluid img-movil">
+            <!--  -->
+            <img src="https://cascoantiguo.temporalweb.es/wp-content/uploads/2023/10/PISCINA_1-1.png" alt="" class="img-fluid img-desktop">
+        </figure>
+        <div class="container-info">
+            <ul class="links">
+                <li>
+                    <a href="#" class="link">Inicio</a>
+                </li>
+                <li>
+                    <a href="#" class="link">Instalaciones</a>
+                </li>
+            </ul>
+            <div class="title-box">
+                <h1 class="title">Instalaciones</h1>
+            </div>
+        </div>
     <?php
 }
