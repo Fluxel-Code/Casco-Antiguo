e880a82d6b9b323b5e4405ae64e8d41c17310a8c
Fluxel-Code <110497050+Fluxel-Code@users.noreply.github.com>
2023-10-10 09:15:03 -0400

    se subio los campos del header

--- SQ-casco-antiguo/wp-content/themes/casco-antiguo/blocks/instalaciones/section-header/callback.php
+++ SQ-casco-antiguo/wp-content/themes/casco-antiguo/blocks/instalaciones/section-header/callback.php
@@ -5,22 +5,44 @@ function FUN_BLOCK_INST_SectionHead_instalaciones____content(){
     //Recuerdar poner esta funcion en "___base"
     ?>
         <figure class="bg-img">
-            <img src="https://cascoantiguo.temporalweb.es/wp-content/uploads/2023/09/PISCINA_1.jpg" alt="" class="img-fluid img-movil">
-            <!--  -->
-            <img src="https://cascoantiguo.temporalweb.es/wp-content/uploads/2023/10/PISCINA_1-1.png" alt="" class="img-fluid img-desktop">
+            <!-- movil -->
+            <?php
+                $image = get_field('ACF_BLOCK_IMG_FIRST_DESCRIPCION_INSTALL');
+                $size = 'full'; // (thumbnail, medium, large, full or custom size)
+                $sClase = "img-fluid img-movil";  // Aqui ponemos las clases de la foto usadas en la maqueta
+
+                if( $image ) {
+                    echo wp_get_attachment_image( $image['id'], $size, false, array("class" => $sClase) );
+                }
+            ?>
+            <!-- desktop -->
+            <?php
+                $image = get_field('ACF_BLOCK_IMG_FIRST_DESCRIPCION_INSTALL');
+                $size = 'full'; // (thumbnail, medium, large, full or custom size)
+                $sClase = "img-fluid img-desktop";  // Aqui ponemos las clases de la foto usadas en la maqueta
+
+                if( $image ) {
+                    echo wp_get_attachment_image( $image['id'], $size, false, array("class" => $sClase) );
+                }
+            ?>
         </figure>
         <div class="container-info">
             <ul class="links">
                 <li>
-                    <a href="#" class="link">Inicio</a>
+                    <a href="#" class="link">
+                        <?php echo get_field("ACF_BLOCK_FIRST_TEXT_START_HEADER_INSTALL"); ?>
+                    </a>
                 </li>
                 <li>
-                    <a href="#" class="link">Instalaciones</a>
+                    <a href="#" class="link">
+                        <?php echo get_field("ACF_BLOCK_SECONDS_TEXT_INSTALL_HEADER_INSTALL"); ?>
+                    </a>
                 </li>
             </ul>
             <div class="title-box">
-                <h1 class="title">Instalaciones</h1>
+                <h1 class="title"><?php echo get_field("ACF_BLOCK_TITLE_HEADER_INSTALL"); ?></h1>
             </div>
         </div>
+        <!-- .container-info -->
     <?php
 }
