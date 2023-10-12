08b4562f9591a1bc7ea36e4e92973d4ea9c1825f
Fluxel-Code <110497050+Fluxel-Code@users.noreply.github.com>
2023-10-12 16:13:09 -0400

    SE SUBIERON LOS BLOQUES

--- SQ-casco-antiguo/wp-content/themes/casco-antiguo/blocks/super-generica/section-consultanos/callback.php
+++ SQ-casco-antiguo/wp-content/themes/casco-antiguo/blocks/super-generica/section-consultanos/callback.php
@@ -4,7 +4,12 @@
 function FUN_BLOCK_SuperG_Consutanos____content(){
     ?>
     
-    <h3>Consultanos</h3>
+        <div class="box-border">
+            <h3 class="title-consult">¿No has encontrado lo que buscabas?</h3>
+            <div class="box-btn">
+                <a href="#" class="SQ_btn btn">Consúltanos</a>
+            </div>
+        </div>
     
     <?php
 }
--- SQ-casco-antiguo/wp-content/themes/casco-antiguo/blocks/super-generica/section-two-images/callback.php
+++ SQ-casco-antiguo/wp-content/themes/casco-antiguo/blocks/super-generica/section-two-images/callback.php
@@ -4,7 +4,34 @@
 function FUN_BLOCK_SuperG_DosFotos____content(){
     ?>
     
-    <h3>Sección dos fotos</h3>
+        <div class="flex-box">
+            <div class="box-info">
+                <h3 class="m-0 title">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultricies</h3>
+                <p class="paragraph">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultricies, odio ac efficitur cursus, ligula neque aliquet massa, non volutpat enim sapien in augue. Maecenas sed facilisis nibh, ac condimentum ipsum. Mauris aliquet, justo et congue tempor, ante orci.
+                </p>
+                <a href="#" class="SQ_btn d-block btn">Call to action button</a>
+            </div>
+            <!-- .box-info -->
+            <div class="box-img">
+                <figure class="m-0">
+                    <img src="https://cascoantiguo.temporalweb.es/wp-content/uploads/2023/10/chica.png" alt="" class="img-fluid">
+                    <div class="layer">
+                        <div class="border"></div>
+                    </div>
+                    <!-- layer -->
+                </figure>
+                <figure class="">
+                    <img src="https://cascoantiguo.temporalweb.es/wp-content/uploads/2023/10/Imagen-22.png" alt="" class="img-fluid">
+                    <div class="layer">
+                        <div class="border"></div>
+                    </div>
+                    <!-- layer -->
+                </figure>
+            </div>
+            <!-- .box-img -->
+        </div>
+        <!-- flex-box -->
     
     <?php
 }
